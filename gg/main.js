1ba6baaf6435664f4eb4e66172d4d5039cf3f8ff
LukasVanHolle <118988674+LukasVanHolle@users.noreply.github.com>
2023-06-07 13:04:04 +0200

    Paden gefixt, hamburgermenu werkt

--- gg/main.js
+++ gg/main.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".menu");
+const icon = document.querySelector(".icon");
+
 hamburger.addEventListener("click", () => {
   nav.setAttribute(
     "aria-expanded",
     nav.getAttribute("aria-expanded") === "true" ? "false" : "true"
   );
+  icon.setAttribute(
+      "aria-hidden",
+          icon.getAttribute("aria-hidden") === "true"? "false" : "true"
+  )
 });
