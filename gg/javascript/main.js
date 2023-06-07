65214d5ad9abe21ef8d1de928798f90b2ade5ace
ChikkyNuggy <vanpuyvelde.henri@gmail.com>
2023-06-07 18:44:16 +0200

    eens wat opgeruimt

--- gg/javascript/main.js
+++ gg/javascript/main.js
@@ -1,35 +1,14 @@
-import Grid from "./Grid.js";
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".menu");
+const icon = document.querySelector(".icon");
 
-const gameBoard = document.querySelector(".game-board");
-const grid = new Grid(gameBoard);
-
-let circles = grid.circles;
-// console.log(circles);
-
-let setWaarden = function (index) {
-  let boven, rechts, onder, links;
-  boven = circles[index].zijdeBoven;
-  rechts = circles[index].zijdeRechts;
-  onder = circles[index].zijdeOnder;
-  links = circles[index].zijdeLinks;
-
-  circles[index].zijdeBoven = links;
-  circles[index].zijdeRechts = boven;
-  circles[index].zijdeOnder = rechts;
-  circles[index].zijdeLinks = onder;
-};
-
-circles.forEach((circle, index) => {
-  circle.cellElement.addEventListener("click", () => {
-    if (circle.kanDraaien(circles, index)) {
-      console.log(index + "mag draaien");
-      let draaiCirkel = circles[index].cellElement;
-      draaiCirkel.style.transform = `rotate(${circles[index].deg + 90}deg)`;
-      circles[index].deg += 90;
-      draaiCirkel.style.transition = `0.5s ease`;
-      setWaarden(index);
-
-      // console.log(circles[index]);
-    }
-  });
+hamburger.addEventListener("click", () => {
+  nav.setAttribute(
+    "aria-expanded",
+    nav.getAttribute("aria-expanded") === "true" ? "false" : "true"
+  );
+  icon.setAttribute(
+      "aria-hidden",
+          icon.getAttribute("aria-hidden") === "true"? "false" : "true"
+  )
 });
---
+++ gg/javascript/mainBoard.js
@@ -0,0 +1,35 @@
+import Grid from "./Grid.js";
+
+const gameBoard = document.querySelector(".game-board");
+const grid = new Grid(gameBoard);
+
+let circles = grid.circles;
+// console.log(circles);
+
+let setWaarden = function (index) {
+  let boven, rechts, onder, links;
+  boven = circles[index].zijdeBoven;
+  rechts = circles[index].zijdeRechts;
+  onder = circles[index].zijdeOnder;
+  links = circles[index].zijdeLinks;
+
+  circles[index].zijdeBoven = links;
+  circles[index].zijdeRechts = boven;
+  circles[index].zijdeOnder = rechts;
+  circles[index].zijdeLinks = onder;
+};
+
+circles.forEach((circle, index) => {
+  circle.cellElement.addEventListener("click", () => {
+    if (circle.kanDraaien(circles, index)) {
+      console.log(index + "mag draaien");
+      let draaiCirkel = circles[index].cellElement;
+      draaiCirkel.style.transform = `rotate(${circles[index].deg + 90}deg)`;
+      circles[index].deg += 90;
+      draaiCirkel.style.transition = `0.5s ease`;
+      setWaarden(index);
+
+      // console.log(circles[index]);
+    }
+  });
+});
